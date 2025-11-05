6cd8c81400516c355baba7493cb7a57740a1ec54
Abhilasha <sadsappyabhi@proton.me>
2025-11-05 03:26:58 -0600

    Added 1 test for swap().
    Test is failing due to inability to dereference pointers as currently implemented. Will need to refactor.

--- arraylist/arraylist.c
+++ arraylist/arraylist.c
@@ -1,34 +1,50 @@
-#include <stdio.h>
-#include <stddef.h>
-#include <string.h>
 #include "arraylist.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    printf("C Implementation of an expandable array a la Java's ArrayList");
-
-    int a = 1;
-    int b = 2;
-
-    int result = testing(a, b);    
-
-    printf("%d", result);
-    
-    return 0;
-}
-
-
-int testing(int a, int b) {
-    return a + b;
-}
-
-// Needed a refresher here on using void* and memcppy
-// to represent a Generic Data Type
-// https://web.stanford.edu/class/archive/cs/cs107/cs107.1258/lectures/8/Lecture8.pdf
+/**
+ * Type-agnostic implementation of swap() using a temporary variable.
+ *
+ * https://web.stanford.edu/class/archive/cs/cs107/cs107.1258/lectures/8/Lecture8.pdf
+ *
+ * @param[in] a, b The two variables to swap data
+ * @param[in] nbytes The number of bytes required to store the data
+ */
 void swap(void *a, void *b, size_t nbytes) {
   char temp[nbytes];
   // void *memcpyvoid(*dest, const void *src, size_t n);
-  memcpy(temp, a, nbytes);
+  memcpy(temp,a, nbytes);
   memmove(a, b, nbytes);
   memcpy(b, a, nbytes);
+}
+
+int main(int argc, char *argv[]) {
+  printf("\nC Implementation of an expandable array a la Java's ArrayList");
+  printf("\n=============================================================\n");
+
+  printf("\nTests of 'swap' helper function:");
+  printf("\n================================\n");
+
+  int a = 4;
+  int b = 3;
+
+  printf("Integer 'A': %d\n", a);
+  printf("Integer 'B': %d\n", b);
+
+  swap(&a, &b, sizeof(a));
+
+  printf("Integer 'A' expected value after swap: 3\n");
+  printf("Integer 'B' expected value after swap: 4\n");
+  printf("Integer 'A' actual value after swap: %d\n", a);
+  printf("Integer 'B' actual value after swap: %d\n", b);
+  
+
+
+
+
+
+
+    
+    return 0;
 }
--- arraylist/arraylist.h
+++ arraylist/arraylist.h
@@ -1,8 +1,9 @@
 #ifndef ARRAYLIST_H
 #define ARRAYLIST_H
-
 /* Initial size arbitrarily chosen to be 10 */
 #define INITIAL_SIZE 10
+
+#include <stddef.h>
 
 typedef struct arraylist {
   void *data[INITIAL_SIZE];
@@ -11,10 +12,10 @@ typedef struct arraylist {
 } Arraylist;
 
 int is_empty(Arraylist *list);
-int add(Arraylist *list, void* data);
-int add_at_index(Arraylist *list, void* data, int index);
+int size(Arraylist *list);
+int add(Arraylist *list, void *data);
+int add_at_index(Arraylist *list, void *data, int index);
 void swap(void *a, void *b, size_t nbytes);
-
 
 int testing(int a, int b);
 
